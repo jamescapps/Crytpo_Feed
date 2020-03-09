d7997c1ea0ff7fa80d24782c40dbffe61cfa30d5
James Capps <jcapps@protonmail.com>
2020-03-09 13:57:00 -0500

    Reduced number of loops used.

--- project/__main__.py
+++ project/__main__.py
@@ -35,69 +35,63 @@ def get_coin():
             print('Coin not found...')
 
         coin = input(f'\nEnter the cryptocurrency you would like to see data on (symbol or name): ')
-        for key, value in res.items():
-            x = [v + z for v in value for z in v]
-            print(x)
-            '''for index, v in value.items():
-                for y, z in v.items():
-                    if z == coin.upper() or z == coin.title():
-                        coin_found = True
-                        show_coin_data(v, coin)'''
+        for coin_id, coin_info in res.items():
+            for data in coin_info.values():
+                if data['name'] == coin.title() or data['symbol'] == coin.upper():
+                    show_coin_data(data, coin)
+                else:
+                    coin_found = False
+                    continue
+                break
+            break
 
-        x = [value + v for value in res for v in value]
-        print(x)
 
-def show_coin_data(item, coin):
-    for k, v in item.items():
-        if k == 'name':
-            print('\n' + k.title() + ': ' + v)
-        if k == 'symbol':
-            print(k.title() + ': ' + v)
-        if k == 'rank':
-            print(k.title() + ': ' + str(v))
-        if k == 'circulating_supply':
-            print(f'Circulating Supply: {int(v):,d} {coin}')
-        if k == 'total_supply':
+def show_coin_data(data, coin):
+    for key, value in data.items():
+        if key == 'name':
+            print('\n' + key.title() + ': ' + value)
+        if key == 'symbol':
+            print(key.title() + ': ' + value)
+        if key == 'rank':
+            print(key.title() + ': ' + str(value))
+        if key == 'circulating_supply':
+            print(f'Circulating Supply: {int(value):,d} {coin}')
+        if key == 'total_supply':
             print(f'Total Supply (Coins in existence minus verified burns): '
-                  f'{int(v):,d} {coin}')
-        if k == 'max_supply' and v != 0:
-            print(f'Max Supply: {int(v):,d} {coin}')
-        if k == 'quotes':
-            for x in v.items():
-                for y in x:
-                    # Run function to get the price data
-                    price_data(y)
-        if k == 'last_updated':
+                  f'{int(value):,d} {coin}')
+        if key == 'max_supply' and value != 0:
+            print(f'Max Supply: {int(value):,d} {coin}')
+        if key == 'quotes':
+            for currency, info in value.items():
+                price_data(info)
+        if key == 'last_updated':
             local_timezone = tzlocal.get_localzone()
-            local_time = (datetime.datetime.fromtimestamp(v, local_timezone)).strftime(
+            local_time = (datetime.datetime.fromtimestamp(value, local_timezone)).strftime(
                 '%y-%m-%d %H:%M:%S')
             print(f'Data Last Updated: {str(local_time)}')
-            # Run next function
             get_another()
 
 
-def price_data(y):
-    if type(y) == dict:
-        for z, a in y.items():
-            if z == 'price':
-                print(f'{z.title()}: ${a:,.2f}')
-            if z == 'volume_24h':
-                print(f'24 Hour Volume: ${a:,.2f}')
-            if z == 'market_cap':
-                print(f'Market Cap: ${a:,.2f}')
-            if z == 'percentage_change_1h':
-                print(f'1 Hour % Change: %{a}')
-            if z == 'percentage_change_24h':
-                print(f'24 Hour % Change: %{a}')
-            if z == 'percentage_change_7d':
-                print(f'7 Day % Change: %{a}')
+def price_data(info):
+    for key, value in info.items():
+        if key == 'price':
+            print(f'{key.title()}: ${value:,.2f}')
+        if key == 'volume_24h':
+            print(f'24 Hour Volume: ${value:,.2f}')
+        if key == 'market_cap':
+            print(f'Market Cap: ${value:,.2f}')
+        if key == 'percentage_change_1h':
+            print(f'1 Hour % Change: %{value}')
+        if key == 'percentage_change_24h':
+            print(f'24 Hour % Change: %{value}')
+        if key == 'percentage_change_7d':
+            print(f'7 Day % Change: %{value}')
 
 
 def get_another():
     while True:
         another = (input('\nWould you like to check another coin?: (y) (n)'))
         if another.lower() == 'y':
-            # Run again
             get_coin()
         elif another.lower() == 'n':
             print('Goodbye!')
